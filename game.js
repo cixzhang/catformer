63d672b7b78ad6c793adadacca11edcb7e8f99e6
henry.olson@ayasdi.com <henry.olson@ayasdi.com>
2017-06-27 18:25:50 -0700

    working tilesets

--- game.js
+++ game.js
@@ -1,3 +1,3 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(400, 288, Phaser.AUTO, '');
 game.state.add('main', mainState);
 game.state.start('main');
--- main-state.js
+++ main-state.js
@@ -3,84 +3,53 @@ var mainState = {
     lastObedient: null,
     lastObedientCheck: null,
 
-    // Function to kill a coin
-    takeCoin: function(player, coin) {
-        coin.kill();
-    },
-
-    // Function to restart the game
-    restart: function() {
-        game.state.start('main');
-    },
-
     preload: function() {  
         // Here we preload the assets
         game.load.image('player', 'assets/sprites/player.png');
         game.load.image('wall', 'assets/sprites/wall.png');
         game.load.image('coin', 'assets/sprites/coin.png');
         game.load.image('lava', 'assets/sprites/lava.png');
+        game.load.image('test_cat', 'assets/sprites/test_cat.png');
+
+        // tilesets
+        game.load.tilemap('tilemap', 'assets/test_map2.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tiles', 'assets/sprites/tileset.png');
     },
 
     create: function() {  
         // Here we create the game
         // Set the background color to blue
-        game.stage.backgroundColor = '#3598db';
+        game.stage.backgroundColor = '#49a';
 
         // Start the Arcade physics system (for movements and collisions)
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        // Add the physics engine to all game objects
-        game.world.enableBody = true;
+        this.map = game.add.tilemap('tilemap');
+        this.map.addTilesetImage('test_tileset', 'tiles');
+
+        this.collisionLayer = this.map.createLayer('Collision');
+        this.backgroundLayer = this.map.createLayer('Background');
+        //this.map.setCollisionBetween(1, 100, true, 'Tile Layer 1');
+    
+        //Before you can use the collide function you need to set what tiles can collide
+        this.map.setCollisionBetween(1, 100, true, 'Collision');
+
+        // sets the size of the game world, doesn't affect the canvas...
+        this.collisionLayer.resizeWorld();
 
         // Variable to store the arrow key pressed
         this.cursor = game.input.keyboard.createCursorKeys();
 
         // Create the player in the middle of the game
-        this.player = game.add.sprite(70, 100, 'player');
+        this.player = game.add.sprite(70, 100, 'test_cat');
+
+        this.game.physics.arcade.enable(this.player);
+
+        // camera
+        this.game.camera.follow(this.player);
 
         // Add gravity to make it fall
         this.player.body.gravity.y = 600;
-
-        // Create 3 groups that will contain our objects
-        this.walls = game.add.group();
-        this.coins = game.add.group();
-        this.hazards = game.add.group();
-
-        // Design the level. x = wall, o = coin, ! = lava.
-        var level = [
-            'xxxxxxxxxxxxxxxxxxxxxx',
-            '!         !          x',
-            '!                 o  x',
-            '!         o          x',
-            '!                    x',
-            '!     o   !    x     x',
-            'xxxxxxxxxxxxxxxx!!!!!x',
-        ];
-
-        // Create the level by going through the array
-        for (var i = 0; i < level.length; i++) {
-            for (var j = 0; j < level[i].length; j++) {
-
-                // Create a wall and add it to the 'walls' group
-                if (level[i][j] == 'x') {
-                    var wall = game.add.sprite(30+20*j, 30+20*i, 'wall');
-                    this.walls.add(wall);
-                    wall.body.immovable = true; 
-                }
-
-                // Create a coin and add it to the 'coins' group
-                else if (level[i][j] == 'o') {
-                    var coin = game.add.sprite(30+20*j, 30+20*i, 'coin');
-                    this.coins.add(coin);
-                }
-
-                // Create a enemy and add it to the 'enemies' group
-                else if (level[i][j] == '!') {
-                    var hazard = game.add.sprite(30+20*j, 30+20*i, 'lava');
-                    this.hazards.add(hazard);
-                }
-            }
-        }
     },
 
     update: function() {
@@ -99,14 +68,7 @@ var mainState = {
             console.log('obedience', cat.obedient);
         }
 
-        // Make the player and the walls collide
-        game.physics.arcade.collide(this.player, this.walls);
-
-        // Call the 'takeCoin' function when the player takes a coin
-        game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this);
-
-        // Call the 'restart' function when the player touches the enemy
-        game.physics.arcade.overlap(this.player, this.hazards, this.restart, null, this);
+        game.physics.arcade.collide(this.player, this.collisionLayer);
 
         // Here we update the game 60 times per second
         if (this.cursor.left.isDown) 
@@ -117,8 +79,9 @@ var mainState = {
             this.player.body.velocity.x = 0;
 
         // Make the player jump if he is touching the ground
-        if (this.cursor.up.isDown && this.player.body.touching.down) {
+        if (this.cursor.up.isDown && this.player.body.blocked.down) {
             this.player.body.velocity.y = -250;
         }
+
     },
 };
