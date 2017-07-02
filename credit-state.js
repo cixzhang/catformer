f347f78e1a94d45a3e5f4d0f38f6fd3d05b649f4
Cindy Zhang <cixzhang@gmail.com>
2017-07-02 09:41:27 -0700

    adding thanks message

--- credit-state.js
+++ credit-state.js
@@ -1,12 +1,18 @@
-
+/* globals Phaser game */
 var creditState = {
   preload: function() {
     // game scaling
     game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
     game.scale.setUserScale(3, 3);
+
+    game.renderer.renderSession.roundPixels = true;
+    Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+
+    game.load.spritesheet('thanks', 'assets/sprites/thanks.png', 200, 200);
   },
   create: function() {
     game.stage.backgroundColor = '#000000';
+    this.thanks = game.add.sprite(33, 0, 'thanks', 0);
     this.startTime = null;
   },
   update: function() {
